67c67c3d7baa442d53c564b215d5e461e5710b88
Mahlatse Boshego-github <166802262+MahlatseBoshego@users.noreply.github.com>
2024-08-28 22:49:56 +0200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,33 @@
+
+# 1. Create an empty list called my_list
+my_list = []
+
+# 2. Append elements to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Insert the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# 4. Extend my_list with another list
+my_list.extend([50, 60, 70])
+
+# 5. Remove the last element from my_list
+my_list.pop()
+#or you could use remove method
+
+
+# 6.Sort my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+print("Index of 30:", my_list.index(30))
+
+# Print the final state of my_list
+print("Final list:", my_list)
+
+
+
+
